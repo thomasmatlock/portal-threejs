99a816b182e5149d7de220b57ad11ab54d67c48a
Thomas Matlock <46877252+thomasmatlock@users.noreply.github.com>
2025-03-12 01:18:37 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,6 @@ import Head from 'next/head';
 export default function Home() {
 	return (
 		<InputContextProvider>
-			<Head>
-				<title>Portal Three.js</title>
-				<meta name="description" content="Portal Three.js" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/portal.svg" />
-			</Head>
 			<Main />
 		</InputContextProvider>
 	);
